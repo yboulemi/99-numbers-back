d669a7387ab324c9112a75920f6a8edc56674742
yboulemi <yanis.boulemia@gmail.com>
2024-04-15 14:14:59 +0200

    updated index.js to use the ssl certs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const sequelize = require('./db/database');
 
 // Import models
@@ -10,38 +13,51 @@ const Round = require('./db/models/Round');
 const Pick = require('./db/models/Pick');
 
 // Import scheduled tasks
-const { setupScheduledTasks } = require('./tasks/taskScheduler'); // Adjust path as necessary
+const { setupScheduledTasks } = require('./tasks/taskScheduler');
 
 // Import routes
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 3252;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-
+// CORS options
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' ? 'https://99-numbers.com' : 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // enabling cookies and auth headers
+  credentials: true,
   allowedHeaders: 'Content-Type,Authorization'
 };
 
 app.use(cors(corsOptions));
-
 app.use('/api', routes);
 
 // Setup scheduled tasks
 setupScheduledTasks();
 
-// Synchronize all models with the database, then start the server
+// Synchronize all models with the database
 sequelize.sync({ alter: true }).then(() => {
   console.log('Database synchronized.');
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
+
+  if (process.env.NODE_ENV === 'production') {
+    // In production, use HTTPS
+    const sslOptions = {
+      key: fs.readFileSync('/etc/ssl/private/99-numbers.com.key.pem'), 
+      cert: fs.readFileSync('/etc/ssl/certs/99-numbers.com.pem') 
+    };
+    https.createServer(sslOptions, app).listen(8443, () => {
+      console.log('Server running on https://99-numbers-com:8443');
+    });
+  } else {
+    // In development, use HTTP
+    const port = process.env.PORT || 3252;
+    http.createServer(app).listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  }
+
 }).catch((error) => {
   console.error('Failed to synchronize database:', error);
 });
